fb8fcdbbd31870dffdd969135b480787cc86ab24
Guilherme Máximo Endo <cochaiendo@gmail.com>
2021-05-17 17:10:44 -0300

    Entendo exceções com C#

---
+++ CSharp-Entendendo_Excecoes/ByteBank/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IO;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CarregarContas();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Catch no metodo Main");
+            }
+
+            Console.WriteLine("Execução finalizada.Tecle Enter para finalizar..");
+            Console.ReadLine();
+        }
+
+        private static void CarregarContas()
+        {
+            using (LeitorDeArquivos leitor = new LeitorDeArquivos("Teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+            //-------------------------------------------------------------
+            //LeitorDeArquivos leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivos("Contas.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Executando finally");
+            //    if(leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+        }
+        private static void TestaInnerException()
+        {
+            try
+            {
+                ContaCorrente conta1 = new ContaCorrente(423, 2342345);
+                ContaCorrente conta2 = new ContaCorrente(534, 5343345);
+
+                //conta1.Transferir(10000, conta2);
+                conta1.Sacar(10000);
+            }
+            catch (OperacaoFinanceiraException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+
+                //Console.WriteLine("Informações da INNER EXCEPTION (exceção interna)");
+
+            }
+        }
+
+        //Teste com a cadeia de chamada:
+        //Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            try
+            {
+                Dividir(10, divisor);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Fui capturado pel(NullReferenceException ex)");
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            /*catch(DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }*/
+
+
+        }
+
+        private static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Exceção com numero = " + numero + " e divisor = " + divisor);
+                throw;
+            }
+
+        }
+    }
+}
